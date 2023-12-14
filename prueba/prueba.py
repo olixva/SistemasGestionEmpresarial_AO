9ae0cdb696d6d507f960167c0f66760ff6851e19
Antonio Oliva Carceles <116766832+olixva@users.noreply.github.com>
2023-12-14 21:43:29 +0100

    first commit

---
+++ prueba/prueba.py
@@ -0,0 +1,22 @@
+
+# Autor: Antonio Oliva Carceles
+# Fecha: 24/11/2023
+# Enunciado: Pruebas
+
+a = 5
+b = "hola"
+c = 9.3
+print(type(a))
+print(a, end="")
+print(" otra");
+
+print("Hola, esto es un numero", a)
+
+x = int(input("Teclee un valor: "))
+print(x, type(x))
+x = str(x)
+print(x, type(x))
+
+# Triples comillas para que respete el formato
+print("""Esto es un texto
+con formato""")
