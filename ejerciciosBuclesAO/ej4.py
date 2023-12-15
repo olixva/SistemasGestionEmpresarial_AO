79cd459cdf75adba3f3da8265bef4395f84786e4
Antonio Oliva Carceles <116766832+olixva@users.noreply.github.com>
2023-12-15 13:21:15 +0100

    Optimizacion EJ4

--- ejerciciosBuclesAO/ej4.py
+++ ejerciciosBuclesAO/ej4.py
@@ -3,8 +3,7 @@ numero = int(input(" Introduce un numero entero positivo "))
 # Recorremos los numeros desde el 0 hasta el numero introducido 
 # si es par lo sumamos a la variable suma
 suma = 0
-for i in range(numero+1):
-    if i%2 == 0:
-        suma += i
+for i in range(0, numero+1, 2):
+    suma += i
 
 print(suma)
