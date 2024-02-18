1d85f0757a0bed73a319c16540b85afbd1f00df3
Antonio Oliva Carceles <116766832+olixva@users.noreply.github.com>
2024-02-19 00:22:36 +0100

    Formateado

--- conjuntos/conjunto.py
+++ conjuntos/conjunto.py
@@ -1,22 +1,22 @@
-curso1=set(["Juan", "Pedro", "Antonio"])
-curso2=set(["Luis", "María", "Lola"])
-curso3=set(["Manolo", "Lucía", "Enrique"])
+curso1 = set(["Juan", "Pedro", "Antonio"])
+curso2 = set(["Luis", "María", "Lola"])
+curso3 = set(["Manolo", "Lucía", "Enrique"])
 
-instituto=[curso1, curso2, curso3]
+instituto = [curso1, curso2, curso3]
 
-equipo=set(["Juan", "Antonio"])
+equipo = set(["Juan", "Antonio"])
 
-#Opcion con for (menos eficiente porque recorre todo el conjunto)
-pertenece=False
+# Opcion con for (menos eficiente porque recorre todo el conjunto)
+pertenece = False
 for curso in instituto:
     if equipo.issubset(curso):
         print("Todo el equipo pertenece a una clase")
-        pertenece=True
-i=0
+        pertenece = True
+i = 0
 
-#Opcion con while (mas eficiente porque para cuando encuentra el elemento)
+# Opcion con while (mas eficiente porque para cuando encuentra el elemento)
 while i < len(instituto) and not equipo.issubset(instituto[i]):
-        i+1
+    i + 1
 
 if i < len(instituto):
     print("pertenece")
